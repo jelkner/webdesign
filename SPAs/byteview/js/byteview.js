08c87be930828ae96dd054b73cbc56fc06b7470a
Jeffrey Elkner <jeff.elkner@gmail.com>
2022-02-28 18:36:43 +0000

    Push latest changes for Mr. Hedgehog

--- SPAs/byteview/js/byteview.js
+++ SPAs/byteview/js/byteview.js
@@ -2,30 +2,25 @@
 window.onload = init;
 
 function init() {
-  var button = document.getElementById("inputButton");
+  const button = document.getElementById('inputButton');
   button.onclick = handleButtonClick;
   addValidation();
 }
 
 function addValidation() {
-  var loc = window.location.href;
-  var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
-  var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+  const loc = window.location.href;
+  let HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+  let CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
                         loc + '?profile=css3';
   document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
   document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
 }
 
 function handleButtonClick(e) {
-  let number = document.getElementById("numberInput").value;
-  let outputP = document.getElementById("output");
+  const number = document.getElementById("numberInput").value;
+  const outputP = document.getElementById("output");
   let outStr = "";
 
-  if (number < 0 || number > 255) {
-    outStr += "Your number, " + number + ", is not in range.";
-    outStr += " Please enter a number between 0 and 255.";
-  } else {
-    outStr += "Your number, " + number + ", is in range.";
-  }
+  outStr += "Your number, " + number + ", is in range. Thanks!";
   outputP.innerHTML = outStr;
 }
