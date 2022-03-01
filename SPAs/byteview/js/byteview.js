a1cabc39471448510a233ce4c3cc1b2b5bd9aa4d
Jeffrey Elkner <jeff.elkner@gmail.com>
2022-03-01 10:54:41 +0000

    Finish byteview except for code page 437

--- SPAs/byteview/js/byteview.js
+++ SPAs/byteview/js/byteview.js
@@ -17,20 +17,57 @@ function addValidation() {
 }
 
 function convertToBase(num, base) {
+  const digits = "0123456789ABCDEF";
   if (num == 0 )
     return "0";
   let conversion = "";
   while (num > 0) {
-    conversion = num % base + conversion;
+    conversion = digits[num % base] + conversion;
     num = Math.floor(num / base);
   }
   return conversion;
+}
+
+function padZeros(numStr, base) {
+  let targetLen = null;
+  if (base == 1)  {
+    targetLen = 8;
+  } else if (base == 8) {
+    targetLen = 3;
+  } else if (base == 16) {
+    targetLen = 2;
+  };
+  while (numStr.length < targetLen) {
+    numStr = "0" + numStr;
+  }
+  return numStr;
+}
+
+function convert8bitToHTMLcolor(num) {
+  const redBits = num >>> 5;
+  const greenBits = (num >>> 2) & 7;
+  const blueBits = num & 3;
+  let color = "#";
+  color += padZeros(convertToBase(Math.floor((redBits / 7) * 255), 16), 16);
+  color += padZeros(convertToBase(Math.floor((greenBits / 7) * 255), 16), 16);
+  color += padZeros(convertToBase(Math.floor((blueBits / 3) * 255), 16), 16);
+  return color;
 }
 
 function processButtonClick(e) {
   const number = document.getElementById("numberInput").value;
   const unsigned = document.getElementById("unsigned");
   unsigned.innerHTML = number;
+  const signed = document.getElementById("signed");
+  signed.innerHTML = (number > 127) ? -1 * (number - 127) : number;
   const bin = document.getElementById("bin");
-  bin.innerHTML = convertToBase(number, 2);
+  bin.innerHTML = padZeros(convertToBase(number, 2), 2);
+  const oct = document.getElementById("oct");
+  oct.innerHTML = padZeros(convertToBase(number, 8), 8);
+  const hex = document.getElementById("hex");
+  hex.innerHTML = padZeros(convertToBase(number, 16), 16);
+  const eightBitColor = document.getElementById("eightBitColor");
+  const color = convert8bitToHTMLcolor(number);
+  eightBitColor.innerHTML = color; 
+  eightBitColor.setAttribute("style", "background-color: " + color);
 }
