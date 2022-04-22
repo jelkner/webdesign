6ad08a47ffdf3bf5ed2ce64f9df9e845040a9d47
Jeffrey Elkner <jeff@elkner.net>
2022-04-22 09:57:13 -0400

    Use JavaScript to add navigation links to arltechCoop site

---
+++ arltechCoop/js/initialization_functions.js
@@ -0,0 +1,21 @@
+function add_validation_links() {
+  let loc = window.location.href;
+  let HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+  let CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                        loc + '?profile=css3';
+  document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+  document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+}
+function add_navigation_links() {
+  let navElement = document.getElementById("common_nav"); 
+  let navLinks = ""
+  let navLinkParts = [
+    ["about.html", "About arlTechCoop"],
+    ["tech.html", "Tech We Use"]
+  ];
+  for (let i = 0; i < navLinkParts.length; i++) {
+    navLinks += '<p><a class="navlinks" href="' + navLinkParts[i][0];
+    navLinks += '">' + navLinkParts[i][1] + '</a></p>\n';
+  }
+  navElement.innerHTML = navLinks;
+}
