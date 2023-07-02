fd4c36afaa05cb982b2e4bc9302a8661e9cefa54
Jeffrey Elkner <jeff.elkner@gmail.com>
2023-07-02 08:51:20 -0400

    Create new nav page for CIW experiments and add audio include example

---
+++ js/init.js
@@ -0,0 +1,8 @@
+function init() {
+  var loc = window.location.href;
+  var HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+  var CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                        loc + '?profile=css3';
+  document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+  document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+}
