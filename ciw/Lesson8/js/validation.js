c52b92fae1645eea5b00673b245d7169e4dcdabe
Jeffrey Elkner <jeff.elkner@gmail.com>
2024-02-20 09:06:35 -0500

    Move form css and js into seperate files

---
+++ ciw/Lesson8/js/validation.js
@@ -0,0 +1,11 @@
+function activateValidationLinks() {
+  let loc = window.location.href;
+  let HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+  let CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                        loc + '?profile=css3';
+  document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+  document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
+  document.getElementById("files").addEventListener(
+    "change", handleFileSelect
+  );
+}
